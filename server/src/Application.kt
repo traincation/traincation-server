a62a6eba18bf046aebb8384c583d8caf1b525ee4
Jonas Schmid <jonas.schmid@gmail.com>
2020-05-21 18:18:57 +0200

    Added api endpoint

--- server/src/Application.kt
+++ server/src/Application.kt
@@ -5,12 +5,11 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.*
 import io.ktor.http.ContentType
-import io.ktor.locations.Location
 import io.ktor.locations.Locations
-import io.ktor.locations.get
 import io.ktor.request.path
 import io.ktor.response.respondText
 import io.ktor.routing.get
+import io.ktor.routing.route
 import io.ktor.routing.routing
 import org.slf4j.event.Level
 import pro.schmid.sbbtsp.Client
@@ -41,49 +40,28 @@ fun Application.module(testing: Boolean = false) {
 
     install(ConditionalHeaders)
 
+    val client = Client()
+
     routing {
         get("/") {
             call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
-        get<MyLocation> {
-            call.respondText("Location: name=${it.name}, arg1=${it.arg1}, arg2=${it.arg2}")
-        }
-        // Register nested routes
-        get<Type.Edit> {
-            call.respondText("Inside $it")
-        }
-        get<Type.List> {
-            call.respondText("Inside $it")
-        }
+        route("api/") {
 
-        get("/solver") {
+            get("/solver") {
 
-            val allPoints = listOf(
-                Station("Yverdon", "8504200"),
-                Station("Stoosbahn", "8577453"),
-                Station("Zernez", "8509262"),
-                Station("Grindelwald", "8505226"),
-                Station("Bâle", "8500010")
-            )
+                val allPoints = listOf(
+                    Station("Yverdon", "8504200"),
+                    Station("Stoosbahn", "8577453"),
+                    Station("Zernez", "8509262"),
+                    Station("Grindelwald", "8505226"),
+                    Station("Bâle", "8500010")
+                )
+                val result = client.solve(allPoints)
 
-            val client = Client()
-            val result = client.solve(allPoints)
-
-            call.respondText(result)
+                call.respondText(result)
+            }
         }
     }
 }
-
-@Location("/location/{name}")
-class MyLocation(val name: String, val arg1: Int = 42, val arg2: String = "default")
-
-@Location("/type/{name}")
-data class Type(val name: String) {
-    @Location("/edit")
-    data class Edit(val type: Type)
-
-    @Location("/list/{page}")
-    data class List(val type: Type, val page: Int)
-}
-
