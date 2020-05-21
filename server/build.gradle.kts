890321bb45bf3aceba34ffe13175ec9e37b6f874
Jonas Schmid <jonas.schmid@gmail.com>
2020-05-21 14:00:23 +0200

    Configure shadowJar

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -5,6 +5,7 @@ val kotlin_version: String by project
 plugins {
     application
     kotlin("jvm") version "1.3.72"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 }
 
 group = "pro.schmid"
