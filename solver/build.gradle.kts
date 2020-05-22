6deb841702f2824b16e36cacc2710d085b9da314
Jonas Schmid <jonas.schmid@gmail.com>
2020-05-22 10:56:25 +0200

    Clean old OrTools data

--- solver/build.gradle.kts
+++ solver/build.gradle.kts
@@ -28,10 +28,6 @@ dependencies {
     implementation("org.jetbrains.exposed", "exposed-jdbc", "0.24.1")
     implementation("org.postgresql:postgresql:42.2.2")
     implementation("com.zaxxer:HikariCP:2.7.8")
-
-    implementation(fileTree("lib") {
-        include("*.jar")
-    })
 }
 
 tasks {
