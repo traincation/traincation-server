736407db8c80689fa606e3b33b605838ebea659b
Jonas Schmid <jonas.schmid@gmail.com>
2020-06-01 13:51:30 +0200

    Removed DAO that is never really used

--- solver/src/main/kotlin/pro/schmid/sbbtsp/repositories/Models.kt
+++ solver/src/main/kotlin/pro/schmid/sbbtsp/repositories/Models.kt
@@ -7,25 +7,10 @@ class Connection(
     val medianDuration: Int
 )
 
-fun pro.schmid.sbbtsp.db.Connection.toRepoModel() = Connection(
-    this.fromStationId.value,
-    this.toStationId.value,
-    this.minDuration,
-    this.medianDuration
-)
-
 data class Station(
     val apiId: String,
     val name: String,
     val latitude: Double,
     val longitude: Double,
     val type: String?
-)
-
-fun pro.schmid.sbbtsp.db.Station.toRepoModel() = Station(
-    this.id.value,
-    this.name,
-    this.latitude,
-    this.longitude,
-    this.type
 )
