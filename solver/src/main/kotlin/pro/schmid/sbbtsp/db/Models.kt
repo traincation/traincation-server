3a22b270509d28bbad384f03cd064626c293b502
Jonas Schmid <jonas.schmid@gmail.com>
2020-06-16 16:24:59 +0200

    Bigger stations list

--- solver/src/main/kotlin/pro/schmid/sbbtsp/db/Models.kt
+++ solver/src/main/kotlin/pro/schmid/sbbtsp/db/Models.kt
@@ -9,7 +9,7 @@ object Connections : IntIdTable() {
     val minDuration = integer("minDuration")
     val medianDuration = integer("medianDuration")
     val lastDownload = long("lastDownload")
-    val stationsList = varchar("stationsList", 500)
+    val stationsList = varchar("stationsList", 1500)
 
     init {
         uniqueIndex(fromStation, toStation)
